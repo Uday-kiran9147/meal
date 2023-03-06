99a6fb7076f5998fe220555b9d165d43247ae085
Uday-kiran9147 <udaykiran9147@gmail.com>
2023-03-06 14:15:29 +0530

    bottom bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,6 +9,16 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Widget> _pages = [CategoriesScreen(), FavoritesScreen()];
+
+  int _selectedPageIndex = 0;
+
+  void selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -17,28 +27,36 @@ class _TabsScreenState extends State<TabsScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.favorite,
-                ),
-                text: 'Favorites',
-              ),
-            ],
-          ),
+          // bottom: TabBar(
+          //   tabs: <Widget>[
+          //     Tab(
+          //       icon: Icon(
+          //         Icons.category,
+          //       ),
+          //       text: 'Categories',
+          //     ),
+          //     Tab(
+          //       icon: Icon(
+          //         Icons.favorite,
+          //       ),
+          //       text: 'Favorites',
+          //     ),
+          //   ],
+          // ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
+        body: _pages[_selectedPageIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.white,
+          onTap: selectPage,
+          items: [
+            BottomNavigationBarItem(
+              label: "Categories",
+              icon: Icon(Icons.category_rounded),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), label: "Favourites")
           ],
+          backgroundColor: Theme.of(context).primaryColor,
         ),
       ),
     );
